30701abaec23ef5fac37d00dddd8037d8b4e8c40
Hiro Protagonist <shd327@gmail.com>
2022-11-08 11:55:19 -0700

    pesudo coded innerHTML card generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 // Create variables 
+var url = "https://opentdb.com/api.php?amount=10&category=15&difficulty=hard&type=multiple"
+// var test = document.getElementById('cardBox')
+// var div = document.createElement('div')
+// var stringTest = "helllo World"
+// div.innerHTML = `<div class="card" style="width: 18rem;">
+//     <img class="card-img-top" src="..." alt="Card image cap">
+//         <div class="card-body">
+//             <h5 class="card-title">${stringTest}</h5>
+//             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//             <a href="#" class="btn btn-primary">Go somewhere</a>
+//         </div>
+// </div>`
+// test.append(div)
