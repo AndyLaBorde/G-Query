a2cb6126b0bdd9dbbf4b2e338c00e7b85ba855c0
Andy LaBorde <32881770+AndyLaBorde@users.noreply.github.com>
2022-11-09 15:14:04 -0700

    fixed color and sizing of search cards, added link to quiz

--- assets/js/index.js
+++ assets/js/index.js
@@ -212,17 +212,16 @@ function RenderCards(data) {
 
         var div = document.createElement('div')
         // console.log(title)
-        div.innerHTML = `<div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="${dimage}" alt="Card image cap">
+        div.innerHTML = `<div class="card m-3 bg-dark" style="width: 18rem; height: 28rem;">
+    <img class="card-img-top custom-height" src="${dimage}" alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title">${title}</h5>
             <p class="card-text">${genre}</p>
             <p class="card-text" id="stars">${rating}</p>
             
             <p class="card-text">${platform}</p>
-   
-             
-              <p class="card-text">${released}</p>
+            
+            <p class="card-text">${released}</p>
             <a href="#" class="btn btn-primary youtube-btn" data-slug="${data.results[i].slug}">YouTube </button>>Go somewhere</a>
         </div>
 </div>`
@@ -233,3 +232,8 @@ function RenderCards(data) {
 
 
 }
+
+$('#toTrivia').on("click", function(){
+    var newWindow = window.open();
+    newWindow.document.location.href = "./quiz.html";
+})
