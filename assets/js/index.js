8b278d123cea1f90c6fac0b26eda7f74d6743b62
Hiro Protagonist <shd327@gmail.com>
2022-11-13 13:29:46 -0700

    added comments and added star icon for rating over 4.5

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ var triviaURL = "https://opentdb.com/api.php?amount=10&category=15&difficulty=ha
 game = "Halo"
 key = "a799090a494c45c5b353265fd2772ec0"
 var rawgIoURL = 'https://api.rawg.io/api/games?search=' + game + '&key=' + key;
-var platformsURL = 'https://api.rawg.io/api/platforms?&key=' + key
+// var platformsURL = 'https://api.rawg.io/api/platforms?&key=' + key
 var genreURL = 'https://api.rawg.io/api/genres?&key=' + key
 var gameURL = 'https://api.rawg.io/api/games?&key=' + key
 var youtubeApiKey = "AIzaSyAwJ4Tla_g2vHjV5OuMWM6QpbxOVMMnz1k"
@@ -13,6 +13,7 @@ var cardboxElement = document.getElementById('cardBox')
 var counter = 0;
 formPanel = document.getElementById('formPanel')
 
+// This function handles fetching based off genre
 function genreFetch(genreURL) {
     fetch(genreURL)
         .then(function (response) {
@@ -23,6 +24,7 @@ function genreFetch(genreURL) {
             RenderCards(data)
         })
 }
+// This function handles fetching based off platform
 function platformFetch(platformURL) {
     fetch(platformURL)
         .then(function (response) {
@@ -32,10 +34,9 @@ function platformFetch(platformURL) {
 
             RenderCards(data)
 
-
-
         })
 }
+// This function handles fetching on a specific game name
 function gameFetch(gameURL) {
 
     fetch(gameURL)
@@ -53,7 +54,7 @@ function gameFetch(gameURL) {
         })
 }
 
-
+// On page load we fetch using rawg.io API to generate card elements of games
 fetch(gameURL)
     .then(function (response) {
         return response.json()
@@ -93,8 +94,6 @@ fetch(gameURL)
             <a href="#" class="btn btn-primary youtube-btn align-item-end hide" id="youtube-btn" data-slug="${data.results[i].slug}">YouTube </button></a>
         </div>
 </div>`
-
-
             cardboxElement.append(div)
             addStar(div)
 
@@ -102,19 +101,28 @@ fetch(gameURL)
 
 
     })
+// This function handles grabs the data-attribute data-star and checks if the value is > 4.5
+// if it meets the condition it appends the star icon to the html element
 function addStar(data) {
+    var starElemnt = `<i class=" fa fa-solid fa-star"></i>`
 
     for (var child of data.children) {
         console.log(child.children[1].children[2].getAttribute('data-star'))
         var star = child.children[1].children[2].getAttribute('data-star')
         if (star > 4.5) {
-            data.children[0].style.border = "solid 5px gold"
+            console.log(data.children[0].children[1].children[2])
+            data.children[0].children[1].children[2].innerHTML = star + " " + " " + starElemnt
+
         }
+
     }
 }
+
 var youtubeButton = document.getElementById('cardBox')
-
-
+// This function listens for a button pressed in a cardbox element with a id of youtube-btn
+// We grab the data-slug and use to the youtube API to fetch the corresponding data
+// We format the data to JSON and parse the return data to grab the videoID
+// After we then launch a youtube video related to the game 
 youtubeButton.addEventListener('click', function (event) {
 
     if (event.target.id == 'youtube-btn') {
@@ -140,7 +148,9 @@ submit.addEventListener('click', function (event) {
     event.preventDefault()
     validateSearch()
 })
-
+// This function handles the search citeria
+// We place a set of conditions so the user can only select one option per a search
+// Depending on the condition we render platformFetch(), genreFetch() or gameFetch() functions
 function validateSearch() {
     var genreOptions = document.getElementById('inputGroupSelect01').value
     var platformOptions = document.getElementById('inputGroupSelect02').value
@@ -168,6 +178,7 @@ function validateSearch() {
     }
 }
 
+// This function handles parse the return data from Rawg.io's API and dynamically generating the html elements
 function RenderCards(data) {
     cardboxElement.innerHTML = ""
     for (let i = 0; i < data.results.length; i++) {
@@ -221,7 +232,7 @@ function RenderCards(data) {
 
 }
 
-
+// Launches the Trivia game
 $('#toTrivia').on("click", function () {
     var newWindow = window.open();
     newWindow.document.location.href = "./quiz.html";
